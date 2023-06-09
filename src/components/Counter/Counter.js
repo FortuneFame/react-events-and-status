42e364aecd6022de53109a42f4e2b834fd6a313e
Marchenko Yaroslava <fortunefame1710@gmail.com>
2023-06-09 03:04:50 +0200

    constructor and state

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,21 +2,30 @@ import React from 'react';
 import './Counter.css';
 
 class Counter extends React.Component {
-    handleIncrement = event => {
-        console.log('+1');
-        console.log(event.target);
-        
+    
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         value: 0,
+    //     }
+    // }
+
+    state = {
+        value: 0,
+    }
+
+    handleIncrement = () => {
+
     };
 
-    handleDecrement = event => {
-        console.log('-1');
-        console.log(event.target);
+    handleDecrement = () => {
+
     };
 
     render() {
         return (
             <div className="Counter">
-                <span className="Counter__value">0</span>
+                <span className="Counter__value">{this.state.value}</span>
                 <div className="Counter__controls">
                     <button type="button" onClick={this.handleIncrement}>
                         Увеличить на 1
